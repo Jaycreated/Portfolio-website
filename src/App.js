9e6d382746fd2411eb5e226b622c2bb6c27a24b3
Jay <johnsonjohnadakole@gmail.com>
2021-11-07 12:15:56 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import React from 'react';
 //import About from './components/About';
 import Contact from './components/Contact';
@@ -7,19 +7,22 @@ import Home from './components/Home'
 //import Main from './components/Main';
 import Navigation from './components/Navigation';
 import Navbar from './components/Navbar';
- 
+
 const App = () => {
-  return (    
-    <Router>
-       <div className=''>
-         <Navbar />
-          <Switch>
-            <Route path='/'<Contact />
-         <Footer />
-    </div>
-   </Router>
-  );
+    return (
+        <Router>
+            <div className=''>
+                <Navbar/>
+                <Switch>
+                    <Route path='/'/>
+                </Switch>
+                <Home />
+                <Contact />
+                <Footer />
+                <Navigation />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
- 
