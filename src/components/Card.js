cba4a038a1b596cf81b385aeba2be0939358e6b1
Jay <johnsonjohnadakole@gmail.com>
2022-01-23 11:03:05 +0100

    updates

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,11 +6,16 @@ const Card = () => {
         <div className = 'w-full'>
            <div className='flex flex-col justify-center max-w-ms mx-auto bg-whtie shadow-xl rounded-xl p-5 '>
                <div className=''>
-                   <img className= 'w-32 mx-auto shadow-xl rounded-full drop-shadow-sm' src={profile} alt = 'profile photo' />
+                   <img className= 'w-32 mx-auto shadow-xl rounded-full drop-shadow-sm' src={profile} alt = 'profile' />
                </div>
                <div className='text-center mt-5'>
                    <p className='text-xl sm:text-2xl  text-gray-900 '>
                        Jay
+                   </p>
+               </div>
+               <div>
+                   <p className='text-xs sm:text-base text-gray-600 pt-2 pb-4 px-5 w-auto inline-block border-b-2 '>
+                       SOFTWARE ENGINEER.
                    </p>
                </div>
            </div>
