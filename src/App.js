d2bd62283ed334fc415e0eab59fbb132f983f027
Jay <johnsonjohnadakole@gmail.com>
2022-06-18 00:13:57 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React from 'react';
 import Body from "./components/Body";
 import Footer from "./components/Footer";
 import Skills from "./components/Skills";
-import Contact from "./components/Contact";
 import Header from './components/Header';
 
 const App = () => {
     return (
             <div>
             <Header />
-            <div className=''>
                 <Body />
+                <div className='md:mx-64'>
                 <Skills />
-                <Contact />
                 <Footer />
             </div>
         </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,36 +1,42 @@
 import React from 'react';
 import Profile from  '../Assets/Calculate.png'
+import locateip from '../Assets/locateip.png';
+import portfolio from '../Assets/portfolio.png';
+import checkref from '../Assets/checkref.png';
+import tech from '../Assets/tech.png';
+import wave from '../Assets/wave.png'
 //import {SiReact, SiTailwindcss, SiJavascript} from 'react-icons/si'
 //import {FaReact,} from 'react-icons/fa'
 
 const Skills = () => {
     return (
         <>
-            <p className='text-center font-bold mt-16 pt-6 border-t-2'>PORTFOLIO</p>
+            <p className='text-center font-bold mt-16 pt-6 border-t-2 text-xl'>PORTFOLIO</p>
+            <p className='text-center tet-sm font-bold animate-pulse'>(click to open)</p>
 
-        <div className='grid md:gap-6 gap-2 grid-cols-3 mt-8 '>
+        <div className='grid md:gap-6 gap-1 grid-cols-3 mt-8 '>
 
-                       <a className='rounded-lg h-32' href=''>
-                           <img className='h-full' src={Profile} alt='calculate' />
+                       <a className='rounded-lg md:h-full h-32' href='https://www.cre8tivetech.com/'>
+                           <img className='h-full' src={tech} alt='calculate' />
                        </a>
 
-                       <a className='rounded-md h-32' href=''>
-                       <img className='h-full' src={Profile} alt='calculate' />
+                       <a className='rounded-md md:h-full h-32' href='https://wavedownloader.com/'>
+                       <img className='h-full' src={wave} alt='calculate' />
                          </a>
 
-                         <a className='rounded-md h-32' href=''>
-                         <img className='h-full' src={Profile} alt='calculate' />
+                         <a className='rounded-md md:h-full h-32' href='https://checkref.co/'>
+                         <img className='h-full' src={checkref} alt='calculate' />
                      </a>
 
-                     <a className='rounded-md h-32' href=''>
-                     <img className='h-full' src={Profile} alt='calculate' />
+                     <a className='rounded-md md:h-full h-32' href='http://jaycreated.me/'>
+                     <img className='h-full' src={portfolio} alt='calculate' />
                  </a>
 
-                 <a className='rounded-md h-32' href=''>
-                 <img className='h-full' src={Profile} alt='calculate' />
+                 <a className='rounded-md md:h-full h-32' href='https://locateip.netlify.app/'>
+                 <img className='h-full' src={locateip} alt='locate ip' />
              </a>
 
-             <a className='rounded-md h-32' href=''>
+             <a className='rounded-md md:h-full h-32' href='https://calculatewithme.netlify.app/'>
              <img className='h-full' src={Profile} alt='calculate' />
          </a>
         </div>
